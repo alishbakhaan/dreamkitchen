8752b79c257325bf6468dbec4bc4a130e6c139fd
alishbakhan <alishbakhaan649@gmail.com>
2024-06-07 10:22:25 +0500

    final

--- src/pages/Section6.jsx
+++ src/pages/Section6.jsx
@@ -17,6 +17,12 @@ import house15 from "../assets/house (15).jpeg"
 import house16 from "../assets/house (16).jpeg"
 import house17 from "../assets/house (17).jpeg"
 import house18 from "../assets/house (18).jpeg"
+import house19 from "../assets/house (19).jpeg"
+import house20 from "../assets/house (20).jpeg"
+import house21 from "../assets/house (21).jpeg"
+import house22 from "../assets/house (22).jpeg"
+import house23 from "../assets/house (23).jpeg"
+import house24 from "../assets/house (24).jpeg"
 
 
 export default function Section6() {
@@ -32,67 +38,67 @@ export default function Section6() {
         </h2>
         <div className="[&_label]:flex-1 gap-8 [&_input]:border-2 [&_input]:border-primary checked:[&_input]:border-[10px] grid grid-cols-3 max-sm:grid-cols-1 mt-6 [&_input]:appearance-none [&_input]:size-8 [&_input]:rounded-full [&_label]:text-[15px] [&_input]:cursor-pointer">
           <div className="flex justify-between items-center gap-4 max-w-60">
-            <img className="rounded-md size-12" src={house1} alt="" />
+            <img className="rounded-md size-12" src={house2} alt="" />
             <label htmlFor="" className="font-medium">Ranch style house</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house2} alt="" />
+          <img className="rounded-md size-12" src={house20} alt="" />
             <label htmlFor="" className="font-medium">Cape cod</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house3} alt="" />
+          <img className="rounded-md size-12" src={house10} alt="" />
             <label htmlFor="" className="font-medium">Mediterranean Revival</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house4} alt="" />
+          <img className="rounded-md size-12" src={house1} alt="" />
             <label htmlFor="" className="font-medium">Townhome</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house5} alt="" />
+          <img className="rounded-md size-12" src={house16} alt="" />
             <label htmlFor="" className="font-medium">Colonial</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house6} alt="" />
+          <img className="rounded-md size-12" src={house9} alt="" />
             <label htmlFor="" className="font-medium">American Craftsmen</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house7} alt="" />
+          <img className="rounded-md size-12" src={house24} alt="" />
             <label htmlFor="" className="font-medium">Farmhouse</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house8} alt="" />
+          <img className="rounded-md size-12" src={house15} alt="" />
             <label htmlFor="" className="font-medium">Tudor</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house9} alt="" />
+          <img className="rounded-md size-12" src={house8} alt="" />
             <label htmlFor="" className="font-medium">Single family home</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house10} alt="" />
+          <img className="rounded-md size-12" src={house23} alt="" />
             <label htmlFor="" className="font-medium">Apartment</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house11} alt="" />
+          <img className="rounded-md size-12" src={house19} alt="" />
             <label htmlFor="" className="font-medium">Tiny home</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house12} alt="" />
+          <img className="rounded-md size-12" src={house7} alt="" />
             <label htmlFor="" className="font-medium">Victorian house</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house13} alt="" />
+          <img className="rounded-md size-12" src={house22} alt="" />
             <label htmlFor="" className="font-medium">Cottage</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
@@ -102,52 +108,52 @@ export default function Section6() {
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house15} alt="" />
+          <img className="rounded-md size-12" src={house6} alt="" />
             <label htmlFor="" className="font-medium">Bungalow</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house16} alt="" />
+          <img className="rounded-md size-12" src={house21} alt="" />
             <label htmlFor="" className="font-medium">Mobile home</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house17} alt="" />
+          <img className="rounded-md size-12" src={house13} alt="" />
             <label htmlFor="" className="font-medium">Contempory</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house18} alt="" />
+          <img className="rounded-md size-12" src={house5} alt="" />
             <label htmlFor="" className="font-medium">Greek Revival</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house1} alt="" />
+          <img className="rounded-md size-12" src={house18} alt="" />
             <label htmlFor="" className="font-medium">Multifamily home</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house2} alt="" />
+          <img className="rounded-md size-12" src={house12} alt="" />
             <label htmlFor="" className="font-medium">Prairie School</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house3} alt="" />
+          <img className="rounded-md size-12" src={house4} alt="" />
             <label htmlFor="" className="font-medium">Split level home style</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house4} alt="" />
+          <img className="rounded-md size-12" src={house17} alt="" />
             <label htmlFor="" className="font-medium">French country</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house5} alt="" />
+          <img className="rounded-md size-12" src={house11} alt="" />
             <label htmlFor="" className="font-medium">Georgian architecture</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
           <div className="flex justify-between items-center gap-4 max-w-60">
-          <img className="rounded-md size-12" src={house6} alt="" />
+          <img className="rounded-md size-12" src={house3} alt="" />
             <label htmlFor="" className="font-medium">Cabin</label>
             <input className="border-[#54a96a]" type="radio" name="architecture" id="" />
           </div>
